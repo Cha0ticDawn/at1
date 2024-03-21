c2cab17bf4888f094fbb11e062047272bdc4d854
Ada Jefferies <ada@Adas-MacBook-Air-2.local>
2024-03-21 17:12:33 +1100

    added logo, audentification and question/answer forms for teachers. I also fixed bugs to do with it

--- eduprod/urls.py
+++ eduprod/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.QuestionCreate.as_view(), name='question_add'),  # URL pattern for adding a question
+    path('list/', views.QuestionList.as_view(), name='question_list'),  # URL pattern for listing questions
+    path('edit/<int:pk>/', views.QuestionUpdate.as_view(), name='question_edit'),  # URL pattern for editing a question
+    path('delete/<int:pk>/', views.QuestionDelete.as_view(), name='question_delete'),  # URL pattern for deleting a question
 ]
--- eduprod/views.py
+++ eduprod/views.py
@@ -1,8 +1,37 @@
 from django.core import serializers
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Question
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from .forms import QuestionForm
 
+@login_required
 def index(request):
     questions = Question.objects.all()
     questions_json = serializers.serialize('json', questions)
     return render(request, 'eduprod/index.html', {'questions_json': questions_json})
+
+class QuestionCreate(LoginRequiredMixin, CreateView):
+    model = Question
+    form_class = QuestionForm
+    template_name = 'eduprod/question_form.html'
+    success_url = reverse_lazy('eduprod:question_list')  # Redirect to the question list page after creating
+
+class QuestionUpdate(LoginRequiredMixin, UpdateView):
+    model = Question
+    form_class = QuestionForm
+    template_name = 'eduprod/question_form.html'
+    success_url = reverse_lazy('eduprod:question_list')  # Redirect to the question list page after updating
+
+class QuestionDelete(LoginRequiredMixin, DeleteView):
+    model = Question
+    template_name = 'eduprod/question_confirm_delete.html'
+    success_url = reverse_lazy('eduprod:question_list')  # Redirect to the question list page after deleting
+
+class QuestionList(LoginRequiredMixin, ListView):
+    model = Question
+    context_object_name = 'questions'
+    template_name = 'eduprod/question_list.html'
